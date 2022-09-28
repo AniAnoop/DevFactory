13f08972f81e16d3c85ffcb8bf2727452ccde00f
AniAnoop <107830221+AniAnoop@users.noreply.github.com>
2022-09-28 16:58:16 +0530

    filter

--- Project/hekto/Frontend/src/pages/Shoppage_gridpage.js
+++ Project/hekto/Frontend/src/pages/Shoppage_gridpage.js
@@ -2,15 +2,21 @@ import React from "react";
 import Header from "../components/Header";
 import Subbanner from "../components/Subbanner";
 import Filterhorizontal from "../components/Filterhorizontal";
+import Gridview from "../components/Gridview";
+import BrandList from "../components/BrandList";
+import Footer from "../components/Footer";
 import "./style.css";
 
 export default function Shoppage_gridpage(){
     return(
         <>
         <div className="shoppage_gridpage">
-            <Header/>
-            <Subbanner/>
+            <Header className="shoppage_gridpage_header"/>
+            {/* <Subbanner/> */}
             <Filterhorizontal/>
+            <Gridview className="shoppage_gridpage_gridview"/>
+            <BrandList/>
+            <Footer/>
         </div>
         </>
     )
